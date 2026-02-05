68e48c50152bac1d66dde1b7b540026fd3869a73
jelee <lje7128@mteletec.com>
2026-02-06 08:44:32 +0900

    useId 훅 추가

--- src/components/html/Checkbox.tsx
+++ src/components/html/Checkbox.tsx
@@ -1,14 +1,17 @@
+import { useId } from "react";
+
 type CheckboxProps = Omit<React.ComponentPropsWithoutRef<"input">, "type"> & {
   type: "checkbox";
   parentClassName: string;
 };
 export default function Checkbox(props: CheckboxProps) {
   const { parentClassName, children, ...rest } = props;
+  const uuid = useId();
   return (
     <>
       <div className={parentClassName}>
-        <input {...rest} />
-        <label>{children}</label>
+        <input id={uuid} {...rest} />
+        <label htmlFor={uuid}>{children}</label>
       </div>
     </>
   );
